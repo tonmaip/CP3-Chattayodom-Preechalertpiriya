b9e1a25a063507583bbef7ef22600f28a14665bb
tonmaip <tonmai.p@gmail.com>
2022-02-10 23:46:17 +0700

    update Exerise4

---
+++ Exercise4_chattayodom_P.py
@@ -0,0 +1,12 @@
+FE_Score = 60
+Gen_Score = 75
+ICS_Score = 65.5
+CP_Score = 60
+
+print("Tonmai College Grade")
+print("--------------------------------------------------")
+print("Foundation English","               :",FE_Score)
+print("General Business","                 :",Gen_Score)
+print("Introduction to Computer Systems"," :",ICS_Score)
+print("Computer Programming","             :",CP_Score)
+print("--------------------------------------------------")
