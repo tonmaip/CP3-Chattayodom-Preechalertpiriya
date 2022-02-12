7fb95772f90e6a4f484886b39d2df8561c0d13ed
tonmaip <tonmai.p@gmail.com>
2022-02-13 00:27:10 +0700

    Update  Ex.8

--- Exercise_8_chattayodom_P.py
+++ Exercise_8_chattayodom_P.py
@@ -18,13 +18,25 @@ if Username == "tm" and Password == "12345":
         manu1 = input("ใส่จำนวนที่ตองการ")
         print("No.1","  ข้าวหมูทอด          " ,manu1 ,manu1*50,"THB")
 
-    if Food == "2":
+    elif Food == "2":
         print("No.2", " ข้าวหมูทอด + ไข่ดาว  ", "65", "THB")
         manu2 = input("ใส่จำนวนที่ตองการ")
         print("No.2", " ข้าวหมูทอด + ไข่ดาว  ", manu2, manu2 * 65, "THB")
 
-    if Food == " ":
-        print("No. ", "   ", "", "THB")
+    elif Food == "3":
+        print("No.3", "ข้าวมันไก่            ", "50", "THB")
         manu3 = input("ใส่จำนวนที่ตองการ")
-        print("No.3", " ข้าวหมูทอด + ไข่ดาว  ", manu3, manu3 * 65, "THB")
+        print("No.3", " ข้าวหมูทอด + ไข่ดาว  ", manu3, manu3 * 50, "THB")
+
+    elif Food == "4":
+        print("No.4", " ข้าวขาหมู           ", "50", "THB")
+        manu4 = input("ใส่จำนวนที่ตองการ")
+        print("No.4", " ข้าวขาหมู           ", manu4, manu4 * 50, "THB")
+
+    elif Food == "5":
+        print("No.5", " น้ำดื้ม ตราสิงโตร้องไห้ ", "10", "THB")
+        manu4 = input("ใส่จำนวนที่ตองการ")
+        print("No.5", " น้ำดื้ม ตราสิงโตร้องไห้ ", manu5, manu5 * 10, "THB")
+
+    else:"Thank You"
 
