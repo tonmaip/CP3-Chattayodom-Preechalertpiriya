e1f2a5e9bc4dbaf1f9582b836af241703145c3ea
tonmaip <tonmai.p@gmail.com>
2022-02-26 00:23:32 +0700

    updateJson Test

---
+++ jsonTest.py
@@ -0,0 +1,34 @@
+import json
+def readJson():
+    # some JSON:
+    x = '{ "name":"John", "age":30, "city":"New York"}'
+
+    # parse x:
+    y = json.loads(x)
+
+    # the result is a Python dictionary:
+    print(y["age"])
+
+def writeJson():
+    import json
+
+    # a Python object (dict):
+    x = {
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    }
+
+    # convert into JSON:
+    y = json.dumps(x)
+
+    # the result is a JSON string:
+    print(y)
+
+readJson()
+writeJson()
+
+file = open("MakeJsonTest","x")
+#ไม่ได้แหะ
+file.write(writeJson(y))
+
